17e3f315d4aef84450e78c2b505d5480d404b0ba
Thet Naing Oo <thetnaingoo1530@gmail.com>
2026-06-21 12:40:50 +0630

    multiple layouts on single interface

--- jfx/demo/src/main/java/com/example/App.java
+++ jfx/demo/src/main/java/com/example/App.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.layout.BorderPane;
@@ -20,11 +22,22 @@ public class App extends Application {
     @Override
     public void start(Stage window) throws IOException {
        BorderPane layout = new BorderPane();
-       layout.setTop(new Label("top"));
-       layout.setRight(new Label("right"));
-       layout.setBottom(new Label("bottom"));
-       layout.setLeft(new Label("left"));
-       layout.setCenter(new Label("center"));
+      
+
+       HBox buttons = new HBox();
+       buttons.setSpacing(10);
+       buttons.getChildren().add(new Button("First"));
+       buttons.getChildren().add(new Button("Second"));
+       buttons.getChildren().add(new Button("Third"));
+
+       VBox texts = new VBox();
+       texts.setSpacing(10);
+       texts.getChildren().add(new Label("First"));
+       texts.getChildren().add(new Label("Second"));
+       texts.getChildren().add(new Label("Third"));
+
+       layout.setTop(buttons);
+       layout.setLeft(texts);
 
        Scene view = new Scene(layout);
 
