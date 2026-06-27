a20c0eae5e331b1e315ad18b78de1e998861485c
Thet Naing Oo <thetnaingoo1530@gmail.com>
2026-06-27 07:24:25 +0630

    input view

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Testing ts = new Testing("Thet Naing Oo");
+        String name = ts.getName();
+        System.out.println(name);
+    }
+}
---
+++ demo/src/main/java/com/example/Testing.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Testing {
+
+    private String testingName;
+
+    public Testing(String name){
+        this.testingName = name;
+    }
+
+    public String getName(){
+        return this.testingName;
+    }
+}
